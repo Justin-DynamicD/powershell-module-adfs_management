38f3267885cf362ee6c46e5391e630123038c8e1
Justin King <justin.dynamicd@gmail.com>
2019-09-12 20:20:01 -0700

    cleanup import

--- adfs-management/functions/Import-ADFSProperties.ps1
+++ adfs-management/functions/Import-ADFSProperties.ps1
@@ -34,8 +34,10 @@
 
   Begin {
     $ErrorActionPreference = "Stop"
+
     # Validate $ADFSContent is in JSON format
     Try {
+      Write-Verbose "importing JSON..."
       $ConvertedContent = ConvertFrom-Json $ADFSContent
     }
     Catch {
@@ -53,31 +55,54 @@
 
   Process {
 
-    # convert JSON into a splat for import
-    Write-Verbose "importing JSON content..."
+    # convert custom objects back into native types if present
+    Write-Verbose "translating TimeSpans..."
+    If ($null -ne $ConvertedContent.ExtranetObservationWindow) {
+      $splat = @{}
+      $ConvertedContent.ExtranetObservationWindow.psobject.properties | ForEach-Object {
+        $splat[$_.Name] = $_.Value 
+      }
+      $ConvertedContent.ExtranetObservationWindow = New-TimeSpan @splat
+    }
 
+    Write-Verbose "translating DateTime..."
+    If ($null -ne $ConvertedContent.PersistentSsoCutoffTime) { 
+      $ConvertedContent.PersistentSsoCutoffTime = Get-Date -Date $tmpValue
+    }
+
+    Write-Verbose "translating Organization..."
+    If ($null -ne $ConvertedContent.OrganizationInfo) {
+      $splat = @{}
+      $ConvertedContent.OrganizationInfo.psobject.properties | ForEach-Object {
+        If ($null -ne $_Value) {
+          $splat[$_.Name] = $_.Value
+        }
+      }
+      $ConvertedContent.OrganizationInfo = New-AdfsOrganization @splat
+    }
+
+    Write-Verbose "translating ContactPerson..."
+    If ($null -ne $ConvertedContent.ContactPerson) {
+      $splat = @{}
+      $ConvertedContent.ContactPerson.psobject.properties | ForEach-Object {
+        If ($null -ne $_Value) {
+          $splat[$_.Name] = $_.Value
+        }
+      }
+      $ConvertedContent.ContactPerson = New-AdfsContactPerson @splat
+    }
+
+    # Some attributes have parameter naming discrepencies.
+    # Do that work here.
     $importSplat = @{}
-    $ConvertedContent.psobject.properties | ForEach-Object { 
-      # some parameters are not simple string imports.
-      # Others have parameter naming discrepencies.
-      # Do that work here.
+    $ConvertedContent.psobject.properties | ForEach-Object {     
       $tmpName = $_.Name
       $tmpValue = $_.Value
       switch ($tmpName) {
         CertificateSharingContainer {} # has no equivelent import value
         ExtranetLockoutEnabled { $importSplat.EnableExtranetLockout = $tmpValue }
-        ExtranetObservationWindow {
-          $timespanSplat = @{
-            Days = $tmpValue.Days
-            Hours = $tmpValue.Hours
-            Minutes = $tmpValue.Minutes
-            Seconds = $tmpValue.Seconds
-          }
-          $importSplat[$tmpName] = New-TimeSpan @timespanSplat
-        }
         KmsiEnabled { $importSplat.EnableKmsi = $tmpValue }
         LoopDetectionEnabled {$importSplat.EnableLoopDetection = $tmpValue }
-        PersistentSsoCutoffTime { $importSplat[$tmpName] = Get-Date -Date $tmpValue }
         PersistentSsoEnabled {$importSplat.EnablePersistentSso = $tmpValue }
         InstalledLanguage {} # has no equivelent import value
         PasswordValidationDelayInMinutes {} # has no equivelent import value
